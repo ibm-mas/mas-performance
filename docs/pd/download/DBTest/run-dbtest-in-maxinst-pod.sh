24f4e89ffa1b70c3b4bdc0e2612c0474063ef61e
Brian Zhu <brianzwh@gmail.com>
2024-08-06 16:55:39 -0400

    [patch] update run-dbtest-in-maxinst-pod.sh to fix schemaowner

--- docs/pd/download/DBTest/run-dbtest-in-maxinst-pod.sh
+++ docs/pd/download/DBTest/run-dbtest-in-maxinst-pod.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# curl -L -v -o run-dbtest-in-maxinst-pod.sh https://ibm-mas.github.io/mas-performance/pd/download/DBTest/run-dbtest-in-maxinst-pod.sh
 
 cd /tmp
 
@@ -18,5 +19,12 @@ export DBUSERNAME="${value}"
 IFS='=' read -r key value <<< $(cat /etc/database/operator/secret/maximo.properties |grep mxe.db.password=)
 export DBPASSWORD="${value}"
 
-export SQLQUERY="select * from maximo.maxattribute"
+IFS='=' read -r key value <<< $(cat /etc/database/operator/secret/maximo.properties |grep mxe.db.schemaowner=)
+export DBSCHEMAOWNER="${value}"
+if [ -z "$DBSCHEMAOWNER" ]; then
+    export SQLQUERY="select * from maxattribute"
+else
+    export SQLQUERY="select * from ${DBSCHEMAOWNER}.maxattribute"
+fi
+
 java -classpath .:/opt/IBM/SMP/maximo/tools/maximo/lib/* "${JAVA_TOOL_OPTIONS}" DBTest
