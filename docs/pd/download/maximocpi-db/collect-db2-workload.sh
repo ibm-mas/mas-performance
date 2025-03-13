c139c4a6f753140e708c63af3751f8ae35139db7
Brian Zhu <bzhu@ca.ibm.com>
2025-03-13 08:41:16 -0400

    [patch] update mcpi

--- docs/pd/download/maximocpi-db/collect-db2-workload.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-workload=$(oc project db2u >/dev/null;oc rsh c-db2wh-manage-db2u-0 bash -c "su - db2inst1 -c 'db2pd -db bludb -transactions member|grep commits'" 2>/dev/null | awk -F ': ' '{print $2}'| sed 's/[[:space:]]*$//')
-echo "# HELP db2_transaction_committed_total Count of db2 committed transactions"
-echo "# TYPE db2_transaction_committed_total counter"
-echo "db2_transaction_committed_total{} ${workload}"
