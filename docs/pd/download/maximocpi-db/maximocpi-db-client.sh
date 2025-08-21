3a4d4e128460e3453a6fe4d2ca653ec8b818e09b
Brian Zhu <bzhu@ca.ibm.com>
2025-08-21 15:40:14 -0400

    [patch] add get manage db metrics script

---
+++ docs/pd/download/maximocpi-db/maximocpi-db-client.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# verify the action parameter
+if [ -z "$1" ]; then 
+    echo "Error: Missing action parameter: -cdf|-cdm|-tl|-es|-h"
+    echo "Info: The command is mas-manage-db-metrics.sh <action parameter>"
+    exit
+fi
+
+# verify if /tmp/db-export exists
+if [ ! -d /tmp/db-export ]; then 
+    mkdir /tmp/db-export; cd /tmp/db-export
+else
+    cd /tmp/db-export
+fi
+
+# verify if maximocpi-db.jar exists
+if [ ! -f /tmp/db-export/maximocpi-db.jar ]; then 
+    curl -L -v -o /tmp/db-export/maximocpi-db.jar https://ibm-mas.github.io/mas-performance/pd/download/maximocpi-db/maximocpi-db.jar > /dev/null 2>&1
+fi
+
+# prepare the execution 
+IFS='=' read -r key value <<< $(cat /etc/database/operator/secret/maximo.properties |grep mxe.db.url=)
+if [[ "${value}" == *"sslConnection=true"* ]]; then
+    export DBURL="${value}sslTrustStoreLocation=${java_truststore};sslTrustStorePassword=${java_truststore_password};"
+else
+    export DBURL="${value}"
+fi
+
+IFS='=' read -r key value <<< $(cat /etc/database/operator/secret/maximo.properties |grep mxe.db.user=)
+export DBUSERNAME="${value}"
+
+IFS='=' read -r key value <<< $(cat /etc/database/operator/secret/maximo.properties |grep mxe.db.password=)
+export DBPASSWORD="${value}"
+
+IFS='=' read -r key value <<< $(cat /etc/database/operator/secret/maximo.properties |grep mxe.db.schemaowner=)
+java -classpath /tmp/db-export/maximocpi-db.jar:/opt/IBM/SMP/maximo/applications/maximo/lib/* "${JAVA_TOOL_OPTIONS}" com.ibm.maximo.mcpi.DBHarmony $1
+
