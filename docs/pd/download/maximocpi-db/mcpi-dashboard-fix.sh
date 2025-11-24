1489afdd0dc54c605251c3d56586881c4d10b7b9
Brian Zhu <bzhu@ca.ibm.com>
2025-11-24 09:06:32 -0500

    [patch] add mcpi 2.1.0 hotfix

--- docs/pd/download/maximocpi-db/mcpi-dashboard-fix.sh
+++ docs/pd/download/maximocpi-db/mcpi-dashboard-fix.sh
@@ -4,8 +4,10 @@
 # curl -L -v -o /tmp/mcpi-dashboard-fix.sh https://ibm-mas.github.io/mas-performance/pd/download/maximocpi-db/mcpi-dashboard-fix.sh > /dev/null 2>&1
 # bash /tmp/mcpi-dashboard-fix.sh
 
+# fix the dashboard wrong directory issue
 cp /opt/app-root/src/exporter/dashboard/mcpi-dashboard.json /opt/app-root/src/ansible/ibm/mas_autoscaling/roles/deploy_dashboards/files/dashboards/mcpi-dashboard.json
 cp /opt/app-root/src/exporter/dashboard/mcpi-dashboard.json /opt/app-root/src/.ansible/collections/ansible_collections/ibm/mas_autoscaling/roles/deploy_dashboards/files/dashboards/mcpi-dashboard.json
 
+# update the sample file with more flexible patterns
 cp /opt/app-root/src/exporter/mas-http-lrq-exporter/rsyslog-http-lrq.sample /opt/app-root/src/exporter/mas-http-lrq-exporter/rsyslog-http-lrq.sample.orig
 curl -L -v -o /opt/app-root/src/exporter/mas-http-lrq-exporter/rsyslog-http-lrq.sample https://ibm-mas.github.io/mas-performance/pd/download/maximocpi-db/rsyslog-http-lrq.sample > /dev/null 2>&1
